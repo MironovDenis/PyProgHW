69acaa967279a6f105a2c229be129e06eee9d236
Denis <mironov_dens@mail.ru>
2023-01-31 20:01:50 +0900

    Внесены правки в код task_2 и task_4

--- HomeWork_1/additional_5.py
+++ HomeWork_1/additional_5.py
@@ -5,5 +5,5 @@ ay = float(input('Введите координаты точки A по оси y
 bx = float(input('Введите координаты точки B по оси x: '))
 by = float(input('Введите координаты точки B по оси y: '))
 
-dist = math.sqrt((ax-bx)**2+(ay-by)**2)
+dist = math.sqrt((ax - bx) ** 2 + (ay - by) ** 2)
 print(f'Растояние между точками A и B = {round(dist, 3)}')
--- HomeWork_1/task_2.py
+++ HomeWork_1/task_2.py
@@ -1,6 +1,5 @@
-seconds = input('Введите время в секундах: ')
-# можно сразу в первой строке перевести секунды в int()
-days = int(seconds) / (3600*24)
-hours = int(seconds) / 3600
-minutes = int(seconds) / 60
+seconds = int(input('Введите время в секундах: '))
+days = seconds / (3600 * 24)
+hours = seconds / 3600
+minutes = seconds / 60
 print(f'Время в формате д:ч:м:с - {days} : {hours} : {minutes} : {seconds}')
